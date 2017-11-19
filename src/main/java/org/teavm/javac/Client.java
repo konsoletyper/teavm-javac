65238f8749cbbec7b50494b9fbaaeba6a26d9ee8
Alexey Andreev <konsoletyper@gmail.com>
2017-11-20 00:44:50 +0300

    Further work on bootstrapping TeaVM

--- src/main/java/org/teavm/javac/Client.java
+++ src/main/java/org/teavm/javac/Client.java
@@ -28,17 +28,27 @@ import javax.tools.JavaCompiler;
 import javax.tools.JavaFileObject;
 import javax.tools.StandardJavaFileManager;
 import org.teavm.backend.javascript.JavaScriptTarget;
+import org.teavm.classlib.impl.JCLPlugin;
 import org.teavm.diagnostics.Problem;
 import org.teavm.diagnostics.ProblemSeverity;
+import org.teavm.interop.Async;
 import org.teavm.jso.JSBody;
+import org.teavm.jso.ajax.XMLHttpRequest;
 import org.teavm.jso.browser.Window;
 import org.teavm.jso.dom.html.HTMLDocument;
 import org.teavm.jso.dom.html.HTMLInputElement;
 import org.teavm.jso.dom.html.HTMLLinkElement;
+import org.teavm.jso.impl.JSOPlugin;
+import org.teavm.jso.typedarrays.ArrayBuffer;
+import org.teavm.jso.typedarrays.Int8Array;
 import org.teavm.jso.typedarrays.Uint8Array;
 import org.teavm.model.ClassHolderSource;
+import org.teavm.model.MethodReference;
+import org.teavm.model.ValueType;
 import org.teavm.parsing.CompositeClassHolderSource;
 import org.teavm.parsing.DirectoryClasspathClassHolderSource;
+import org.teavm.platform.async.AsyncCallback;
+import org.teavm.platform.plugin.PlatformPlugin;
 import org.teavm.vm.TeaVM;
 import org.teavm.vm.TeaVMBuilder;
 
@@ -48,6 +58,7 @@ public final class Client {
 
     public static void main(String[] args) throws IOException {
         createStdlib();
+        loadTeaVMClasslib();
 
         HTMLDocument document = Window.current().getDocument();
         HTMLInputElement textArea = (HTMLInputElement) document.getElementById("source-code");
@@ -117,22 +128,28 @@ public final class Client {
 
         List<ClassHolderSource> classSources = new ArrayList<>();
         classSources.add(new DirectoryClasspathClassHolderSource(new File("/out")));
-        classSources.add(new DirectoryClasspathClassHolderSource(new File("/stdlib"), stdlibMapping));
+        classSources.add(new DirectoryClasspathClassHolderSource(new File("/teavm-stdlib"), stdlibMapping));
 
         TeaVM teavm = new TeaVMBuilder(new JavaScriptTarget())
                 .setClassSource(new CompositeClassHolderSource(classSources))
                 .build();
+        new JSOPlugin().install(teavm);
+        new PlatformPlugin().install(teavm);
+        new JCLPlugin().install(teavm);
 
+        teavm.entryPoint("main", new MethodReference("Hello", "main", ValueType.parse(String[].class),
+                ValueType.VOID))
+                .withArrayValue(1, "java.lang.String");
         File outDir = new File("/js-out");
         outDir.mkdirs();
         teavm.build(outDir, "classes.js");
         boolean hasSevere = false;
         for (Problem problem : teavm.getProblemProvider().getProblems()) {
-            System.out.println(problem.getSeverity() + " " + problem.getText());
             if (problem.getSeverity() == ProblemSeverity.ERROR) {
                 hasSevere = true;
             }
         }
+        TeaVMProblemRenderer.describeProblems(teavm);
 
         return !hasSevere;
     }
@@ -145,26 +162,66 @@ public final class Client {
         ClassLoader loader = ClassLoader.getSystemClassLoader();
 
         try (ZipInputStream input = new ZipInputStream(loader.getResourceAsStream("stdlib/data.zip"))) {
-            while (true) {
-                ZipEntry entry = input.getNextEntry();
-                if (entry == null) {
-                    break;
-                }
+            unzip(input, baseDir);
+        }
+    }
 
-                File outputFile = new File(baseDir, entry.getName());
-                outputFile.getParentFile().mkdirs();
-                try (OutputStream output = new FileOutputStream(outputFile)) {
-                    byte[] buffer = new byte[65536];
-                    while (true) {
-                        int bytesRead = input.read(buffer);
-                        if (bytesRead < 0) {
-                            break;
-                        }
-                        output.write(buffer, 0, bytesRead);
-                    }
-                }
+    private static void loadTeaVMClasslib() throws IOException {
+        File baseDir = new File("/teavm-stdlib");
+        baseDir.mkdirs();
+
+        byte[] data = downloadFile("teavm-classlib.zip");
+        try (ZipInputStream input = new ZipInputStream(new ByteArrayInputStream(data))) {
+            unzip(input, baseDir);
+        }
+    }
+
+    private static void unzip(ZipInputStream input, File toDir) throws IOException {
+        while (true) {
+            ZipEntry entry = input.getNextEntry();
+            if (entry == null) {
+                break;
+            }
+            if (entry.isDirectory()) {
+                continue;
+            }
+
+            File outputFile = new File(toDir, entry.getName());
+            outputFile.getParentFile().mkdirs();
+            try (OutputStream output = new FileOutputStream(outputFile)) {
+                copy(input, output);
             }
         }
+    }
+
+    private static void copy(InputStream input, OutputStream output) throws IOException {
+        byte[] buffer = new byte[1024];
+        while (true) {
+            int bytesRead = input.read(buffer);
+            if (bytesRead < 0) {
+                break;
+            }
+            output.write(buffer, 0, bytesRead);
+        }
+    }
+
+    @Async
+    private static native byte[] downloadFile(String url);
+
+    private static void downloadFile(String url, AsyncCallback<byte[]> callback) {
+        XMLHttpRequest xhr = XMLHttpRequest.create();
+        xhr.open("GET", url, true);
+        xhr.setResponseType("arraybuffer");
+        xhr.onComplete(() -> {
+            ArrayBuffer buffer = (ArrayBuffer) xhr.getResponse();
+            Int8Array jsArray = Int8Array.create(buffer);
+            byte[] array = new byte[jsArray.getLength()];
+            for (int i = 0; i < array.length; ++i) {
+                array[i] = jsArray.get(i);
+            }
+            callback.complete(array);
+        });
+        xhr.send();
     }
 
     private static void createSourceFile(String content) throws IOException {
---
+++ src/main/java/org/teavm/javac/TeaVMProblemRenderer.java
@@ -0,0 +1,92 @@
+/*
+ *  Copyright 2017 Alexey Andreev.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.teavm.javac;
+
+import java.util.Iterator;
+import org.teavm.callgraph.CallGraph;
+import org.teavm.callgraph.CallGraphNode;
+import org.teavm.callgraph.CallSite;
+import org.teavm.diagnostics.DefaultProblemTextConsumer;
+import org.teavm.diagnostics.Problem;
+import org.teavm.model.CallLocation;
+import org.teavm.model.MethodReference;
+import org.teavm.model.TextLocation;
+import org.teavm.vm.TeaVM;
+
+public final class TeaVMProblemRenderer {
+    private TeaVMProblemRenderer() {
+    }
+
+    public static void describeProblems(TeaVM vm) {
+        CallGraph cg = vm.getDependencyInfo().getCallGraph();
+        DefaultProblemTextConsumer consumer = new DefaultProblemTextConsumer();
+        for (Problem problem : vm.getProblemProvider().getProblems()) {
+            consumer.clear();
+            problem.render(consumer);
+            StringBuilder sb = new StringBuilder();
+            sb.append(consumer.getText());
+            renderCallStack(cg, problem.getLocation(), sb);
+            String problemText = sb.toString();
+            switch (problem.getSeverity()) {
+                case ERROR:
+                    System.out.println("ERROR: " + problemText);
+                    break;
+                case WARNING:
+                    System.out.println("WARNING: " + problemText);
+                    break;
+            }
+        }
+    }
+
+    public static void renderCallStack(CallGraph cg, CallLocation location, StringBuilder sb) {
+        if (location == null) {
+            return;
+        }
+        sb.append("\n    at ");
+        renderCallLocation(location.getMethod(), location.getSourceLocation(), sb);
+        if (location.getMethod() != null) {
+            CallGraphNode node = cg.getNode(location.getMethod());
+            for (int i = 0; i < 100; ++i) {
+                Iterator<? extends CallSite> callSites = node.getCallerCallSites().iterator();
+                if (!callSites.hasNext()) {
+                    break;
+                }
+                CallSite callSite = callSites.next();
+                sb.append("\n    at ");
+                renderCallLocation(callSite.getCaller().getMethod(), callSite.getLocation(), sb);
+                node = callSite.getCaller();
+            }
+        }
+    }
+
+    public static void renderCallLocation(MethodReference method, TextLocation location, StringBuilder sb) {
+        if (method != null) {
+            sb.append(method.getClassName() + "." + method.getName());
+        } else {
+            sb.append("unknown method");
+        }
+        if (location != null) {
+            sb.append("(");
+            String fileName = location.getFileName();
+            if (fileName != null) {
+                sb.append(fileName.substring(fileName.lastIndexOf('/') + 1));
+                sb.append(':');
+            }
+            sb.append(location.getLine());
+            sb.append(')');
+        }
+    }
+}
